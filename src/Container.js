0428bcb75ae03af317912a707fe223213a34fa75
Dimitri C <couwenberg@gmail.com>
2019-04-25 09:41:40 +1000

    showing second listbox

--- src/Container.js
+++ src/Container.js
@@ -3,43 +3,54 @@ import React, { Component } from "react";
 import DebtType from "./DebtType";
 import mockOptions from "./mockData.json";
 import ClearDebtType from "./ClearDebt";
-import { reduxForm } from "redux-form";
+import { reduxForm,formValueSelector } from "redux-form";
 import { connect } from "react-redux";
 
 class MyContainer extends Component {
-    handleChangeDebtType = event => {
-      //save selected debttype to store
-      console.log("handleChangeDebtType value",event.target.value);
-     //this.props.setDebtType(event.target.value);
-     this.props.change("debtType", event.target.value);
-    };
-  
-    render() {
-      return (
-        <div>
-          <DebtType
-            options={mockOptions.DEBT_TYPE}
-            handleChangeDebtType={this.handleChangeDebtType}
-          />
-          <ClearDebtType options={mockOptions.CLEARDEBT_TYPE} />
-        </div>
-      );
-    }
-  }
-  
+  handleChangeDebtType = event => {
+    //save selected debttype to store
+    console.log("handleChangeDebtType value", event.target.value);
+    //this.props.setDebtType(event.target.value);
+    this.props.change("debtType", event.target.value);
+  };
 
-const mapDispatchToProps = state => dispatch => ({
-  //selectedDebtType:
-  setDebtType: type => dispatch(change('facilityForm','debtType',type))
+  render() {
+    const {debtType} = this.props;
+
+    console.log('debtType',debtType)
+    return (
+      <div>
+        <DebtType
+          options={mockOptions.DEBT_TYPE}
+          handleChangeDebtType={this.handleChangeDebtType}
+        />
+
+        {debtType}
+        {(debtType === "1" || debtType === "2") && (
+          <ClearDebtType options={mockOptions.CLEARDEBT_TYPE} />
+        )}
+      </div>
+    );
+  }
+}
+
+const selector = formValueSelector('facilitiesForm');
+const mapStateToProps = state => ({
+  debtType: selector(state, "debtType")
 });
 
+// const mapDispatchToProps = state => dispatch => ({
+//   //selectedDebtType:
+//   setDebtType: type => dispatch(change('facilityForm','debtType',type))
+// });
+
 MyContainer = connect(
-  undefined,
-  mapDispatchToProps
+  mapStateToProps,
+  undefined
 )(MyContainer);
 
 export default reduxForm({
-  form: "simple" // a unique identifier for this form
+  form: "facilitiesForm" // a unique identifier for this form
 })(MyContainer);
 
 //export default ;
