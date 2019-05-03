0f0d67367e7d5ca8df55ffa445e9b21882d880f6
Dimitri C <couwenberg@gmail.com>
2019-05-03 18:24:52 +1000

    created connectedContainer for test

--- src/Container.js
+++ src/Container.js
@@ -6,7 +6,7 @@ import ClearDebtType from "./ClearDebt";
 import { reduxForm, formValueSelector, Field } from "redux-form";
 import { connect } from "react-redux";
 
-class MyContainer extends Component {
+ export class MyContainer extends Component {
   handleChangeDebtType = event => {
     //save selected debttype to store
     console.log("handleChangeDebtType value", event.target.value);
@@ -33,16 +33,8 @@ class MyContainer extends Component {
     }
   };
 
-  // disableNewLimit = (val) => {
-  //   console.log('newlimitdisabled')
-  //   if (val === 4 || val === 5) {
-  //     return true;
-  //   }
-  //   return false;
-  // };
-
   render() {
-    const { debtType, newLimit, currentLimit } = this.props;
+  const { debtType, newLimit, currentLimit } = this.props;
 
     console.log("debtType", debtType);
 
@@ -50,6 +42,7 @@ class MyContainer extends Component {
 
     return (
       <div>
+
         <DebtType
           options={mockOptions.DEBT_TYPE}
           handleChangeDebtType={this.handleChangeDebtType}
@@ -58,8 +51,6 @@ class MyContainer extends Component {
         {(debtType === "1" || debtType === "2") && (
           <ClearDebtType options={mockOptions.CLEARDEBT_TYPE} />
         )}
-
-        {debtType}
 
         <Field name="currentLimit" component="input" type="number" />
         <Field
@@ -80,18 +71,11 @@ const mapStateToProps = state => ({
   newLimit: selector(state, "newLimit")
 });
 
-// const mapDispatchToProps = state => dispatch => ({
-//   //selectedDebtType:
-//   setDebtType: type => dispatch(change('facilityForm','debtType',type))
-// });
-
-MyContainer = connect(
+const ConnectedContainer = connect(
   mapStateToProps,
   undefined
 )(MyContainer);
 
 export default reduxForm({
   form: "facilitiesForm" // a unique identifier for this form
-})(MyContainer);
-
-//export default ;
+})(ConnectedContainer);
---
+++ src/Container.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import  {shallow, configure } from 'enzyme'
+import {MyContainer}  from './Container'
+import Adapter from 'enzyme-adapter-react-16';
+
+
+configure({ adapter: new Adapter() });
+
+describe('Test Button component', () => {
+    it('Test click event', () => {
+        const defaultProps = {debtType:"1"}
+        const myComp = shallow(<MyContainer {...defaultProps}/>);
+        // wrapper.find('DebtType').simulate('click');
+
+        // const result = wrapper.find('ClearDebtType')
+
+        // expect(result.length).toEqual(0)
+    });
+  });
+
+// describe('Container', () => {
+//   const defaultProps = {debtType:"1"}
+//   const wrapper = shallow(<Container {...defaultProps} />)
+
+//   it('cleardebttype should show', () => {
+
+//     wrapper.find('DebtType').simulate('click');
+
+//     const result = wrapper.find('ClearDebtType')
+
+//     expect(result.length).toEqual(0)
+
+
+//   })
+// })
