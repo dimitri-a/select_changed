25c689a9bc03d100e4f98766ccd09b72be0bf5f8
Dimitri C <couwenberg@gmail.com>
2019-05-03 19:41:49 +1000

    how to find element with enzyme??

--- src/Container.test.js
+++ src/Container.test.js
@@ -6,8 +6,7 @@ configure({ adapter: new Adapter() });
 
 describe('Container component', () => {
     it('should show the second select component', () => {
-        const defaultProps = {debtType:"1"}
-        const myComp = shallow(<MyContainer debtType={1}/>);
+        const myComp = shallow(<MyContainer debtType={"1"}/>);
         //question: how can I find the second select in the browser?
         //https://airbnb.io/enzyme/docs/api/ReactWrapper/find.html
         const result = myComp.find('#root > div > div > select:nth-child(2)')   
