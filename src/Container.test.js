ccebfb8ecb79942dffef6fce5bf81e5f6a85793a
Dimitri C <couwenberg@gmail.com>
2019-05-03 21:57:19 +1000

    test working

--- src/Container.test.js
+++ src/Container.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import  {shallow, configure } from 'enzyme'
 import {MyContainer}  from './Container'
 import Adapter from 'enzyme-adapter-react-16';
+import ClearDebt from "./ClearDebt";
 configure({ adapter: new Adapter() });
 
 describe('Container component', () => {
@@ -9,8 +10,8 @@ describe('Container component', () => {
         const myComp = shallow(<MyContainer debtType={"1"}/>);
         //question: how can I find the second select in the browser?
         //https://airbnb.io/enzyme/docs/api/ReactWrapper/find.html
-        const result = myComp.find('#root > div > div > select:nth-child(2)')   
-        console.log('result',result.length)
+        const result = myComp.find(ClearDebt);
+        console.log('result',result.props().options.length)
         expect(result.length ).toEqual(1)
     });
   });
