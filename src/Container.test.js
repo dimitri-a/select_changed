0c06e8f4da7bc52d32a8da1161144e06a35215cb
Dimitri C <couwenberg@gmail.com>
2019-05-04 10:42:37 +1000

    test for newlimit 0 update

--- src/Container.test.js
+++ src/Container.test.js
@@ -35,12 +35,18 @@ describe('Container select visibility tests', () => {
     
     it('should default newlimit to zero when debType is Clear close or Clear and convert | 312-ac3 ', () => {
         //when debtType is Clear close/Clear and convert
-        const myComp = shallow(<MyContainer debtType={"4"}/>);
+        const myComp = shallow(<MyContainer/>);
         const result =myComp.instance().updateNewLimit("4",2000)
-     
-        console.log(result)
            //expect the result to  be 0
         expect(result).toEqual(0)
+    });
+
+    it('should default newlimit to currentLimit when debType is No Change, Variation, Disbursement Only | 312-ac3 ', () => {
+        //when debtType is Clear close/Clear and convert
+        const myComp = shallow(<MyContainer/>);
+        const result =myComp.instance().updateNewLimit("3",2000)
+           //expect the result to  be 2000
+        expect(result).toEqual(2000)
     });
 
 
